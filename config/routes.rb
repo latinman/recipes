62520eaca5bf53b8fb2d3e968d4762de1718886c
Ernesto Rivas <erivas78@gmail.com>
2015-08-23 20:40:40 -0400

    Added admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :chefs, except: [:new]
+  resources :chefs, except: [:new, :destroy]
   get "/register" => "chefs#new"
   get "/login" => "logins#new"
   post "/login" => "logins#create"
